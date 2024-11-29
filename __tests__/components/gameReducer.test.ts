8dd4bae980b1220e309e0e557a3397af52ac2ca8
skamranahmed <syedkamranahmed14@gmail.com>
2024-11-30 01:00:45 +0530

    add gameReducer

---
+++ __tests__/components/gameReducer.test.ts
@@ -0,0 +1,31 @@
+import { Tile } from "@/models/tiles";
+import gameReducer, { initialState } from "@/reducers/gameReducer";
+import { act, renderHook } from "@testing-library/react";
+import { useReducer } from "react";
+
+describe("gameReducer", () => {
+  describe("create_tile", () => {
+    it("should create a new tile", () => {
+      const tile: Tile = {
+        position: [0, 0],
+        value: 2,
+      };
+
+      const { result } = renderHook(() => useReducer(gameReducer, initialState));
+      const [, dispatch] = result.current;
+
+      act(() =>
+        dispatch({
+          type: "create_tile",
+          tile: tile,
+        }),
+      );
+
+      const [state] = result.current;
+
+      expect(state.board[0][0]).toBeDefined();
+
+      expect(Object.values(state.tiles)).toEqual([tile]);
+    });
+  });
+});
