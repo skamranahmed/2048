a069f2627e6497c12d4282bb32f8fab5f74d52ce
skamranahmed <syedkamranahmed14@gmail.com>
2024-11-29 21:06:53 +0530

    add Board component

---
+++ components/board.tsx
@@ -0,0 +1,20 @@
+import styles from "@/styles/board.module.css";
+
+export default function Board() {
+  const renderGrid = () => {
+    const cells: JSX.Element[] = [];
+    const totalCellsCount = 16; // 4x4 grid
+
+    for (let index = 0; index < totalCellsCount; index++) {
+      cells.push(<div className={styles.cell} key={index}></div>);
+    }
+
+    return cells;
+  };
+
+  return (
+    <div className={styles.board}>
+      <div className={styles.grid}>{renderGrid()}</div>
+    </div>
+  );
+}
