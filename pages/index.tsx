50fa18fc1272ad659a1c9e738d967678c9c851ba
skamranahmed <syedkamranahmed14@gmail.com>
2024-12-07 22:20:57 +0530

    update css and tile colours

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
 import Board from "@/components/board";
 import Score from "@/components/score";
+import styles from "@/styles/index.module.css";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.twenty48}>
       <Head>
         <title>2048</title>
         <meta name="description" content="2048 game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <header>
+        <h1>2048</h1>
         <Score />
+      </header>
+      <main>
         <Board />
       </main>
-    </>
+      <footer>Made with ❤️ by Kamran</footer>
+    </div>
   );
 }
